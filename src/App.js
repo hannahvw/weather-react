76f4abe79d858f70371cce0fcea95cc95a159232
hannahvw <103385894+hannahvw@users.noreply.github.com>
2024-02-29 09:33:58 -0600

    Added github link

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Weather />
+        <p className="attribute">
+          Code by{" "}
+          <a
+            href="https://github.com/hannahvw/weather-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Hannah Van Wyk
+          </a>
+        </p>
       </header>
     </div>
   );
